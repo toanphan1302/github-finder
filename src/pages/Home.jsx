f33e0f076d6bd94b859a26d119760d573be0fb40
Toan Phan <toanphan1302@gmail.com>
2022-04-13 08:42:54 -0700

    add_all_files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResult from "../components/users/UserResult"
+import UserSearch from "../components/users/UserSearch"
+
+function Home() {
+  return (
+    <div>
+      <UserSearch />
+      <UserResult />
+    </div>
+  )
+}
+
+export default Home
